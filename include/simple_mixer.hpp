ea5f92afc02894915c9e275f959b7c95257c0093
Thies Lennart Alff <lennart.alff@gmail.com>
2022-09-23 15:00:14 +0000

    simple mixer and actuator controls mixer

--- include/simple_mixer.hpp
+++ include/simple_mixer.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 namespace hippo_control {
+namespace mixer {
 static constexpr int kChannels = 8;
 enum class Channel {
   kRoll,
@@ -15,13 +16,13 @@ enum class Channel {
 };
 
 struct Mapping {
-  double limits[kChannels] = {0.0};
-  double scalings[kChannels] = {0.0};
+  std::array<double, kChannels> limits{};
+  std::array<double, kChannels> scalings{};
 };
 
 struct Output {
   double total = 0.0;
-  double channels[kChannels] = {0.0};
+  std::array<double, kChannels> channels{};
 };
 
 class SimpleMixer {
@@ -33,13 +34,13 @@ class SimpleMixer {
 
   std::array<double, kChannels> Mix(
       const std::array<double, kChannels> &_actuator_controls);
-  private:
 
+ private:
   Mapping mappings_[kChannels];
   Output outputs_[kChannels];
-  void ApplyInput(const std::array<double, kChannels> &_actuator_controls);
-  void ScaleToLimits();
+  double ApplyInput(const std::array<double, kChannels> &_actuator_controls);
+  void ScaleOutputs(double _scale);
   void ResetOutputs();
 };
-
+}  // namespace mixer
 }  // namespace hippo_control
--- src/simple_mixer.cpp
+++ src/simple_mixer.cpp
@@ -1,31 +1,60 @@
 #include "simple_mixer.hpp"
 
-using namespace hippo_control;
+#include <math.h>
+
+namespace hippo_control {
+namespace mixer {
 
 SimpleMixer::SimpleMixer(){};
 
 void SimpleMixer::SetMapping(int _index, const Mapping &_mapping) {
-  if (!(_index < kChannels)) {
+  if ((_index >= kChannels)) {
     return;
   }
   mappings_[_index] = _mapping;
 }
 
 void SimpleMixer::ResetOutputs() {
-  for (Output &output: outputs_) {
+  for (Output &output : outputs_) {
     output.total = 0.0;
-    for (double channel: output.channels) {
-      channel = 0.0;
-    }
+    output.channels.fill(0.0);
   }
 }
 
-void SimpleMixer::ApplyInput(
+double SimpleMixer::ApplyInput(
     const std::array<double, kChannels> &_actuator_controls) {
   ResetOutputs();
+  double scaling = 0.0;
   for (int i_out = 0; i_out < kChannels; ++i_out) {
     for (int i_in = 0; i_in < kChannels; ++i_in) {
-      outputs_[i_out].total += _actuator_controls[i_in] * mappings_[i_out].scalings[i_in];
+      double tmp = _actuator_controls[i_in] * mappings_[i_out].scalings[i_in];
+      outputs_[i_out].total += tmp;
+      outputs_[i_out].channels[i_in] += tmp;
     }
+    double tmp = abs(outputs_[i_out].total);
+    scaling = std::max(tmp, scaling);
+  }
+  return scaling;
+}
+
+void SimpleMixer::ScaleOutputs(double _scale) {
+  for (Output &output : outputs_) {
+    output.total /= _scale;
   }
 }
+
+std::array<double, kChannels> SimpleMixer::Mix(
+    const std::array<double, kChannels> &_actuator_controls) {
+  double scale = ApplyInput(_actuator_controls);
+  if (scale > 1.0) {
+    ScaleOutputs(scale);
+  }
+  std::array<double, kChannels> out;
+  for (int i = 0; i < kChannels; ++i) {
+    out[i] = outputs_[i].total;
+  }
+  return out;
+}
+
+}  // namespace mixer
+}  // namespace hippo_control
