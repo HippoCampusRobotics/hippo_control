5e3a5e780ea747366ce0b2f2af352912f35fec87
Thies Lennart Alff <lennart.alff@gmail.com>
2022-09-29 13:46:07 +0000

    complete attitude_controller implementation

--- src/nodes/attitude_control_node.cpp
+++ src/nodes/attitude_control_node.cpp
@@ -1,3 +1,4 @@
+#include <eigen3/Eigen/Dense>
 #include <hippo_common/param_utils.hpp>
 #include <hippo_msgs/msg/actuator_controls.hpp>
 #include <hippo_msgs/msg/attitude_target.hpp>
@@ -28,27 +29,94 @@ class AttitudeControlNode : public rclcpp::Node {
     rcl_interfaces::msg::ParameterDescriptor descr;
     std::string descr_text;
 
-    name = "p_gains";
-    descr_text = "Proportional gains for roll, pitch and yaw.";
-    descr = param_utils::Description(descr_text);
+    name = "feedthrough";
+    descr_text =
+        "Directly feed through roll, pitch and yaw without using any control "
+        "law.";
+    descr = param_utils::Description(descr_text, true);
     {
       std::lock_guard<std::mutex> lock(mutex_);
-      p_gains_ = std::vector<double>({1.0, 1.0, 1.0});
-      p_gains_ = declare_parameter(name, p_gains_, descr);
-      controller_.SetPgains(p_gains_);
+      feedthrough_ = false;
+      feedthrough_ = declare_parameter(name, feedthrough_, descr);
     }
 
-    name = "d_gains";
-    descr_text = "Derivate gains for the vehicle's body rates.";
+    name = "gain.roll.p";
+    descr_text = "Proportional gain for roll.";
     descr = param_utils::Description(descr_text);
     {
       std::lock_guard<std::mutex> lock(mutex_);
-      d_gains_ = std::vector<double>({0.1, 0.1, 0.1});
-      d_gains_ = declare_parameter(name, d_gains_, descr);
-      controller_.SetDgains(d_gains_);
+      gain_roll_p_ = 1.0;
+      gain_roll_p_ = declare_parameter(name, gain_roll_p_, descr);
+      controller_.SetRollGainP(gain_roll_p_);
     }
-    paramter_callback_handle_ = add_on_set_parameters_callback(
-        std::bind(&AttitudeControlNode::OnSetParameters, this, _1));
+
+    name = "gain.pitch.p";
+    descr_text = "Proportional gain for pitch.";
+    descr = param_utils::Description(descr_text);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      gain_pitch_p_ = 1.0;
+      gain_pitch_p_ = declare_parameter(name, gain_pitch_p_, descr);
+      controller_.SetPitchGainP(gain_pitch_p_);
+    }
+
+    name = "gain.yaw.p";
+    descr_text = "Proportional gain for yaw.";
+    descr = param_utils::Description(descr_text);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      gain_yaw_p_ = 1.0;
+      gain_yaw_p_ = declare_parameter(name, gain_yaw_p_, descr);
+      controller_.SetYawGainP(gain_yaw_p_);
+    }
+
+    name = "gain.roll.d";
+    descr_text = "Derivative gain for roll.";
+    descr = param_utils::Description(descr_text);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      gain_roll_d_ = 1.0;
+      gain_roll_d_ = declare_parameter(name, gain_roll_d_, descr);
+      controller_.SetRollGainD(gain_roll_d_);
+    }
+
+    name = "gain.pitch.d";
+    descr_text = "Derivative gain for pitch.";
+    descr = param_utils::Description(descr_text);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      gain_pitch_d_ = 1.0;
+      gain_pitch_d_ = declare_parameter(name, gain_pitch_d_, descr);
+      controller_.SetPitchGainD(gain_pitch_d_);
+    }
+
+    name = "gain.yaw.d";
+    descr_text = "Derivative gain for yaw.";
+    descr = param_utils::Description(descr_text);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      gain_yaw_d_ = 1.0;
+      gain_yaw_d_ = declare_parameter(name, gain_yaw_d_, descr);
+      controller_.SetYawGainD(gain_yaw_d_);
+    }
+
+    name = "scale_output";
+    descr_text =
+        "If true, the output gets evenly to be in range[-1, 1]. Otherwise it "
+        "gets clipped";
+    descr = param_utils::Description(descr_text);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      scale_output_ = false;
+      scale_output_ = declare_parameter(name, scale_output_, descr);
+    }
+
+    misc_cb_handle_ = add_on_set_parameters_callback(
+        std::bind(&AttitudeControlNode::OnSetMisc, this, _1));
+    p_gains_cb_handle_ = add_on_set_parameters_callback(
+        std::bind(&AttitudeControlNode::OnSetPgains, this, _1));
+    d_gains_cb_handle_ = add_on_set_parameters_callback(
+        std::bind(&AttitudeControlNode::OnSetDgains, this, _1));
   }
 
   void InitPublishers() {
@@ -57,6 +125,9 @@ class AttitudeControlNode : public rclcpp::Node {
 
     topic = "actuator_control";
     control_output_pub_ = create_publisher<ActuatorControls>(topic, qos);
+
+    topic = "~/current_setpoint";
+    setpoint_pub_ = create_publisher<AttitudeTarget>(topic, qos);
   }
 
   void InitSubscriptions() {
@@ -74,49 +145,168 @@ class AttitudeControlNode : public rclcpp::Node {
   }
 
   void OnAttitudeTarget(const AttitudeTarget::SharedPtr _msg) {
+    if (_msg->header.frame_id != "map") {
+      RCLCPP_WARN_THROTTLE(
+          get_logger(), *get_clock(), 1000,
+          "AttitudeTarget frame is [%s] but only [map] is handled. Ignoring...",
+          _msg->header.frame_id.c_str());
+    }
     std::lock_guard<std::mutex> lock(mutex_);
-    attitude_target_ = *_msg;
+    if (!(_msg->mask & _msg->IGNORE_ROLL_ANGLE)) {
+      attitude_target_.attitude.x = _msg->attitude.x;
+    }
+    if (!(_msg->mask & _msg->IGNORE_PITCH_ANGLE)) {
+      attitude_target_.attitude.y = _msg->attitude.y;
+    }
+    if (!(_msg->mask & _msg->IGNORE_YAW_ANGLE)) {
+      attitude_target_.attitude.z = _msg->attitude.z;
+    }
+    if (!(_msg->mask & _msg->IGNORE_ROLL_RATE)) {
+      attitude_target_.body_rate.x = _msg->body_rate.x;
+    }
+    if (!(_msg->mask & _msg->IGNORE_PITCH_RATE)) {
+      attitude_target_.body_rate.y = _msg->body_rate.y;
+    }
+    if (!(_msg->mask & _msg->IGNORE_YAW_RATE)) {
+      attitude_target_.body_rate.z = _msg->body_rate.z;
+    }
+    if (!(_msg->mask & _msg->IGNORE_THRUST)) {
+      attitude_target_.thrust = _msg->thrust;
+    }
+    controller_.SetAngularVelocityTarget(attitude_target_.body_rate.x,
+                                         attitude_target_.body_rate.y,
+                                         attitude_target_.body_rate.z);
+    controller_.SetOrientationTarget(attitude_target_.attitude.x,
+                                     attitude_target_.attitude.y,
+                                     attitude_target_.attitude.z);
+    setpoint_pub_->publish(attitude_target_);
+    if (feedthrough_) {
+      ActuatorControls msg;
+      msg.header.stamp = now();
+      msg.control[msg.INDEX_ROLL] = attitude_target_.attitude.x;
+      msg.control[msg.INDEX_PITCH] = attitude_target_.attitude.y;
+      msg.control[msg.INDEX_YAW] = attitude_target_.attitude.z;
+      msg.control[msg.INDEX_THRUST] = attitude_target_.thrust;
+      control_output_pub_->publish(msg);
+    }
   }
 
   void OnOdometry(const Odometry::SharedPtr _msg) {
-    // TODO(lennartalff): Either update the controller directly in here or just
-    // store the message and update the controller in an synchronuous loop
+    if (feedthrough_) {
+      return;
+    }
+    const auto &q_ros = _msg->pose.pose.orientation;
+    const auto &omega = _msg->twist.twist.angular;
+
+    Eigen::Quaterniond orientation{q_ros.w, q_ros.x, q_ros.y, q_ros.z};
+    Eigen::Vector3d v_angular{omega.x, omega.y, omega.z};
+
+    ActuatorControls msg;
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      Eigen::Vector3d u =
+          controller_.Update(orientation, v_angular, scale_output_);
+      msg.control[ActuatorControls::INDEX_ROLL] = u.x();
+      msg.control[ActuatorControls::INDEX_PITCH] = u.y();
+      msg.control[ActuatorControls::INDEX_YAW] = u.z();
+      msg.control[ActuatorControls::INDEX_THRUST] = attitude_target_.thrust;
+    }
+    control_output_pub_->publish(msg);
   }
 
-  SetParametersResult OnSetParameters(
+  SetParametersResult OnSetMisc(
       const std::vector<rclcpp::Parameter> &parameters) {
     SetParametersResult result;
-
+    result.successful = true;
     for (const rclcpp::Parameter &parameter : parameters) {
       std::lock_guard<std::mutex> lock(mutex_);
 
-      if (param_utils::AssignIfMatch(parameter, "p_gains", p_gains_)) {
-        controller_.SetPgains(p_gains_);
+      if (param_utils::AssignIfMatch(parameter, "scale_output",
+                                     scale_output_)) {
         continue;
       }
-      if (param_utils::AssignIfMatch(parameter, "d_gains", d_gains_)) {
-        controller_.SetDgains(d_gains_);
-        continue;
-      }
-
-      RCLCPP_WARN(get_logger(), "Unhandled parameter was set: [%s]",
-                  parameter.get_name().c_str());
     }
+    return result;
+  }
+
+  SetParametersResult OnSetPgains(
+      const std::vector<rclcpp::Parameter> &parameters) {
+    SetParametersResult result;
     result.successful = true;
+    for (const rclcpp::Parameter &parameter : parameters) {
+      std::lock_guard<std::mutex> lock(mutex_);
+      if (param_utils::AssignIfMatch(parameter, "gain.roll.p", gain_roll_p_)) {
+        controller_.SetRollGainP(gain_roll_p_);
+        continue;
+      }
+      if (param_utils::AssignIfMatch(parameter, "gain.pitch.p",
+                                     gain_pitch_p_)) {
+        controller_.SetPitchGainP(gain_pitch_p_);
+        continue;
+      }
+      if (param_utils::AssignIfMatch(parameter, "gain.yaw.p", gain_yaw_p_)) {
+        controller_.SetYawGainP(gain_yaw_p_);
+        continue;
+      }
+    }
+    return result;
+  }
+  SetParametersResult OnSetDgains(
+      const std::vector<rclcpp::Parameter> &parameters) {
+    SetParametersResult result;
+    result.successful = true;
+    for (const rclcpp::Parameter &parameter : parameters) {
+      std::lock_guard<std::mutex> lock(mutex_);
+      if (param_utils::AssignIfMatch(parameter, "gain.roll.d", gain_roll_d_)) {
+        controller_.SetRollGainD(gain_roll_d_);
+        continue;
+      }
+      if (param_utils::AssignIfMatch(parameter, "gain.pitch.d", gain_yaw_d_)) {
+        controller_.SetPitchGainD(gain_yaw_d_);
+        continue;
+      }
+      if (param_utils::AssignIfMatch(parameter, "gain.yaw.d", gain_yaw_d_)) {
+        controller_.SetYawGainD(gain_yaw_d_);
+        continue;
+      }
+    }
     return result;
   }
 
  private:
   std::mutex mutex_;
-  std::vector<double> p_gains_;
-  std::vector<double> d_gains_;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // ros params
+  //////////////////////////////////////////////////////////////////////////////
+  double gain_roll_p_;
+  double gain_roll_d_;
+  double gain_pitch_p_;
+  double gain_pitch_d_;
+  double gain_yaw_p_;
+  double gain_yaw_d_;
+  bool scale_output_;
+
   GeometricAttitudeControl controller_;
   AttitudeTarget attitude_target_;
 
+  bool feedthrough_;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // publisher
+  //////////////////////////////////////////////////////////////////////////////
   rclcpp::Publisher<ActuatorControls>::SharedPtr control_output_pub_;
+  rclcpp::Publisher<AttitudeTarget>::SharedPtr setpoint_pub_;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // subscriber
+  //////////////////////////////////////////////////////////////////////////////
   rclcpp::Subscription<AttitudeTarget>::SharedPtr target_sub_;
   rclcpp::Subscription<Odometry>::SharedPtr odometry_sub_;
-  OnSetParametersCallbackHandle::SharedPtr paramter_callback_handle_;
+
+  OnSetParametersCallbackHandle::SharedPtr p_gains_cb_handle_;
+  OnSetParametersCallbackHandle::SharedPtr d_gains_cb_handle_;
+  OnSetParametersCallbackHandle::SharedPtr misc_cb_handle_;
 };
 
 int main(int argc, char **argv) {
